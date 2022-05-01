7974d44faaad05dfc726d337d73150a482670776
Mariya <mariya1285@gmail.com>
2022-05-01 11:05:43 -0400

    updated files

--- import_sql.py
+++ import_sql.py
@@ -1,7 +1,14 @@
 #import required packages
 import pandas as pd
 import psycopg2
-
+import numpy
+from psycopg2.extensions import register_adapter, AsIs
+def addapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+def addapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+register_adapter(numpy.float64, addapt_numpy_float64)
+register_adapter(numpy.int64, addapt_numpy_int64)
 # cursor.executemany()
 
 #connect to the database
@@ -21,28 +28,31 @@ dataframe = pd.read_csv("C:/Users/Mariya/OneDrive/Desktop/Spring22/DMQL/DMQL-pro
 insert_lst = list()
 #iterate over the data rows in order to get the insert query
 #created the blueprint of the query and generated the data to be populated trhough query
+insert_str = "INSERT INTO data VALUES("
+insert_list = list()
 for i in range(640):
-    insert_str = "INSERT INTO data VALUES("
-    insert_str+=str(dataframe['District code'][i])+','+'\''+str(dataframe['State name'][i])\
-                +'\',\''+str(dataframe['District name'][i])+'\','+str(dataframe['Population'][i])+\
-                    ','+str(dataframe['Male'][i])+','+str(dataframe['Female'][i])+','+str(dataframe['Literate'][i])\
-                    +','+str(dataframe['Male_Literate'][i])+','+str(dataframe['Female_Literate'][i])+','+str(dataframe['Male_Workers'][i])\
-                    +','+str(dataframe['Female_Workers'][i])+','+str(dataframe['Frontline_Workers'][i])+\
-                    ','+str(dataframe['Industrial_Workers'][i])+\
-                    ','+str(dataframe['Non_Workers'][i])+','+str(dataframe['Cultivator_Workers'][i])+','+str(dataframe['Agricultural_Workers'][i])\
-                    +','+str(dataframe['Household_Workers'][i])+','+str(dataframe['Other_Workers'][i])+','+str(dataframe['LPG_or_PNG_Households'][i])+','+\
-                        str(dataframe['Housholds_with_Electric_Lighting'][i])\
-                    +','+str(dataframe['Households_with_Internet'][i])+','+str(dataframe['Households_with_Computer'][i])+\
-                    ','+str(dataframe['Rural_Households'][i])+','+str(dataframe['Urban_Households'][i])+','+str(dataframe['Below_Primary_Education'][i])\
-                    +','+str(dataframe['Primary_Education'][i])+','+str(dataframe['Middle_Education'][i])+','+str(dataframe['Secondary_Education'][i])\
-                    +','+str(dataframe['Higher_Education'][i])+','+str(dataframe['Graduate_Education'][i])+','+str(dataframe['Other_Education'][i])+\
-                    ','+str(dataframe['Age_Group_0_29'][i])+','+str(dataframe['Age_Group_30_49'][i])+','+str(dataframe['Age_Group_50'][i])\
-                    +','+str(dataframe['Age not stated'][i])\
-                    +');'
-    #pythonic way to generate the queries to be used on Postgres DB data insertion
-    # print(insert_str)
-    cursor.execute(insert_str)
+    
+    insert_list.append(tuple([str(dataframe['District code'][i]), str(dataframe['State name'][i]),\
+                    str(dataframe['District name'][i]),dataframe['Population'][i],\
+                    dataframe['Male'][i],dataframe['Female'][i],dataframe['Literate'][i],\
+                    dataframe['Male_Literate'][i],dataframe['Female_Literate'][i],dataframe['Male_Workers'][i],\
+                    dataframe['Female_Workers'][i],dataframe['Frontline_Workers'][i],\
+                    dataframe['Industrial_Workers'][i],\
+                    dataframe['Non_Workers'][i],dataframe['Cultivator_Workers'][i],dataframe['Agricultural_Workers'][i],\
+                    dataframe['Household_Workers'][i],dataframe['Other_Workers'][i],dataframe['LPG_or_PNG_Households'][i],\
+                        dataframe['Housholds_with_Electric_Lighting'][i],\
+                    dataframe['Households_with_Internet'][i],dataframe['Households_with_Computer'][i],\
+                    dataframe['Rural_Households'][i],dataframe['Urban_Households'][i],dataframe['Below_Primary_Education'][i],\
+                    dataframe['Primary_Education'][i],dataframe['Middle_Education'][i],dataframe['Secondary_Education'][i],\
+                    dataframe['Higher_Education'][i],dataframe['Graduate_Education'][i],dataframe['Other_Education'][i],\
+                    dataframe['Age_Group_0_29'][i],dataframe['Age_Group_30_49'][i],dataframe['Age_Group_50'][i],\
+                    dataframe['Age not stated'][i]
 
+
+                    ]))
+    #pythonic way to generate the queries to be used on Postgres DB data insertion
+print(insert_list[0])
+cursor.executemany("INSERT INTO data VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", insert_list)
 
 conn.commit()
 conn.close()
